ea045375a12b7f9fee577a835a2eb279e02da54d
Petter Remen <petter.remen@meltwater.com>
2017-07-20 13:53:59 +0200

    Use spring boot

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,7 +1,24 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
 public class Application {
+    @RequestMapping("/")
+    public Map<String, String> hello() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("greeting", "Hello World!");
+        return map;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/test/java/com/example/ApplicationTest.java
+++ src/test/java/com/example/ApplicationTest.java
@@ -1,11 +1,29 @@
 package com.example;
 
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
     @Test
     public void running_main_does_not_crash() throws Exception {
-        Application.main(new String[]{});
+        ResponseEntity<Map> response = testRestTemplate.getForEntity("/", Map.class);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertThat(response.getBody().get("greeting"), equalTo("Hello World"));
     }
 }
