677d911e68fd26df92e3570c8fa035a6b806dc92
Petter Remen <petter.remen@meltwater.com>
2017-07-20 12:34:49 +0200

    Add Application and ApplicationTest

---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Test
+    public void running_main_does_not_crash() throws Exception {
+        Application.main(new String[]{});
+    }
+}
